096c71d17aae36a470377c657c40d73932a9b1d4
William <william2958@betatest.com>
2018-01-18 15:48:40 -0500

    Added non-mutability changes

--- test/Unhappy_Path/03_nation_policy_exception.test.js
+++ test/Unhappy_Path/03_nation_policy_exception.test.js
@@ -29,4 +29,15 @@ contract('Nation Policy Exception Testing', accounts => {
 			})
 	});
 
+	it('Should fail to set a nations policy twice', function() {
+		return nationInstance.SetNationPolicy(1, "nation code", "nationcodelink", "lawEM", true, {from: accounts[0]})
+			.then(function(txReceipt) {
+				return nationInstance.SetNationPolicy(1, "nation code 2", "nationcodelink2", "lawEM2", true, {from: accounts[0]});
+			})
+			.then(assert.fail)
+			.catch(function(error) {
+				assert(error.message.indexOf('revert') >= 0, "error should be revert");
+			})
+	})
+
 });
